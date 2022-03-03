995d9ebd2f955c98b0e918c827c373059d385404
landofcoder <landofcoder@gmail.com>
2022-03-04 00:05:46 +0700

    up

--- Cron/RunImporter.php
+++ Cron/RunImporter.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * Venustheme
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the Venustheme.com license that is
  * available through the world-wide-web at this URL:
  * http://www.venustheme.com/license-agreement.html
- * 
+ *
  * DISCLAIMER
- * 
+ *
  * Do not edit or add to this file if you wish to upgrade this extension to newer
  * version in the future.
- * 
+ *
  * @category   Venustheme
  * @package    Ves_Blog
  * @copyright  Copyright (c) 2020 Venustheme (http://www.venustheme.com/)
@@ -50,7 +50,7 @@ class RunImporter extends \Magento\Backend\App\Action
     ) {
         $this->helper = $helper;
         $this->importerCollectionFactory = $importerCollectionFactory;
-        $this->_importerLogFactory = $_importerLogFactory;
+        $this->_importerLogFactory = $importerLogFactory;
         parent::__construct($context);
     }
 
@@ -83,7 +83,7 @@ class RunImporter extends \Magento\Backend\App\Action
                         $modelLog->setData($data);
                         $modelLog->save();
                     }catch (\Exception $e) {
-                        
+
                     }
                 }
             }
